105ecb3b615da2ac4e85e8df401e4bed41715e3d
jiatong0216 <240851942@qq.com>
2019-04-20 10:42:00 +0800

    登录绘制引入elementUI

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>登录登录</h1>
+  <div class="login_container">
+    <div class="box">
+      <img src="../assets/images/logo.png" alt="">
+    </div>
   </div>
 </template>
 
@@ -11,4 +13,24 @@ export default {
 </script>
 
 <style  scoped>
+.login_container {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(45deg, #ccc, rgba(0, 217, 255, 0.685))
+}
+.login_container .box {
+  width: 400px;
+  height: 250px;
+  border: 1px solid #fff;
+  box-shadow: 0 0 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-60%);
+  background: linear-gradient(45deg, rgb(96, 161, 145), #eee)
+}
+.login_container .box img {
+  margin: 0;
+  padding: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 依赖全局样式文件
+import './assets/css/global.css'
+
+// 依赖element-UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+// 控制台的日志是否详细
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
