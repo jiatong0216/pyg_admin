8621836cbf2dc8205ac241f71688423f30efb4c8
jiatong0216 <240851942@qq.com>
2019-04-26 19:03:21 +0800

    参数分类

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -1,16 +1,25 @@
 <template>
-    <div>
-
-    </div>
+  <div class="params_container">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-alert title="注意:第三级分类才可设置参数" type="warning" show-icon></el-alert>
+      <el-form-item label="选择商品分类：">
+          <el-cascader></el-cascader>
+      </el-form-item>
+    </el-card>
+  </div>
 </template>
 
 <script>
-import mixin from './Params-Mixin'
+import mixin from "./Params-Mixin";
 export default {
-    mixins: [mixin]    
-}
+  mixins: [mixin]
+};
 </script>
 
 <style scoped>
-
 </style>
